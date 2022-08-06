c66334da7c8f30d56ca801211b8e5c168ff744cc
aa-ag <aaron.aguerrevere@gmail.com>
2022-08-06 17:01:31 -0500

    Add helper function read_csvs()

--- etl.py
+++ etl.py
@@ -2,13 +2,17 @@ from email.mime import base
 
 
 ############------------ IMPORTS ------------##################################
-
-
-############------------ GLOBAL VARIABLE(S) ------------#######################
+import csv
 
 
 ############------------ FUNCTION(S) ------------##############################
+def read_csvs(path_to_csv):
+    with open(path_to_csv, newline='') as csvfile:
+        datareader = csv.reader(csvfile, delimiter=' ', quotechar='|')
 
+        for row in datareader:
+            print(', '.join(row))
+    
 
 ############------------ DRIVER CODE ------------##############################ß
 if __name__ == "__main__":
