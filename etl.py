8d28b0488bc273e304fb3c96e12b80f4f53b6aad
aa-ag <aaron.aguerrevere@gmail.com>
2022-08-06 17:13:07 -0500

    Add helper function to print headers

--- etl.py
+++ etl.py
@@ -23,9 +23,16 @@ def consume_csv_data():
     path_to_csv = "constructors.csv"
     
     data = read_csvs(path_to_csv)
+    print_headers(data)
     
+
+def print_headers(data):
     for row in data:
-        print(", ".join(row))
+        headers = ", ".join(row)
+        print(f"\nThere are {len(headers)} columns on this file.")
+        print("\nHere's a list of all headers:")
+        print(headers.split(","),"\n")
+        return
 
 
 ############------------ DRIVER CODE ------------##############################ß
