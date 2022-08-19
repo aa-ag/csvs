e4383e07205c66a1824473a314483a08ac0f36a1
aa-ag <aaron.aguerrevere@gmail.com>
2022-08-19 16:39:32 -0500

    Add silly first unit test to test container & cdci

--- spa/checks/tests.py
+++ spa/checks/tests.py
@@ -1,3 +1,11 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
-# Create your tests here.
+class TestViews(TestCase):
+    def is_home_html_rendered(self):
+        '''
+         python manage.py test checks.tests.TestViews.is_home_html_rendered
+        '''
+        client = Client()
+        response = self.client.get('')
+        print("\nStatus code:", response.status_code)
+        self.assertEqual(response.status_code, 200)
