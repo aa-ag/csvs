c89dfa2e765d6b8bf77456dabfae1a644f28ae7f
aaron <aaron.aguerrevere@zendesk.com>
2022-08-28 15:38:20 -0500

    Remove cron, move docker-compose & remove .circleci for now

--- spa/spa/cron.py
+++
@@ -1,6 +0,0 @@
-import os
-
-def delete_samples_cron_job():
-    samples_directory = "spa/static/samples"
-    for sample in os.listdir(samples_directory):
-        os.remove(os.path.join(samples_directory, sample))
