5e9c2c8e77989eb1581cf97d9d8ca4b1c63c05b5
aa-ag <aaron.aguerrevere@gmail.com>
2022-09-03 12:05:45 -0500

    Add docstrings to both Views tests

--- spa/checks/tests.py
+++ spa/checks/tests.py
@@ -6,6 +6,8 @@ from checks.views import home
 class TestViews(TestCase):
     def test_if_root_url_resolves_to_home(self):
         '''
+         Check if Django is getting the correct
+         view it expects when calling the site's root
         '''
         found = resolve('/')
         self.assertEqual(found.func, home)
@@ -13,6 +15,7 @@ class TestViews(TestCase):
 
     def test_if_home_template_renders(self):
         '''
+         Check if the home view renders OK
         '''
         response = self.client.get('')
         self.assertEqual(response.status_code, 200)
