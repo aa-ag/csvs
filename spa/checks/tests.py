b89002fb771f83658e450b3989fd656104a068b9
aa-ag <aaron.aguerrevere@gmail.com>
2022-09-03 11:56:16 -0500

    Rename home views unittest

--- spa/checks/tests.py
+++ spa/checks/tests.py
@@ -3,10 +3,19 @@ from django.test import TestCase
 ### Unit tests
 ### --- VIEWS -----------------------------------------------------------------
 class TestViews(TestCase):
-    def is_home_html_rendered(self):
+    def test_if_home_template_renders(self):
         '''
          python manage.py test checks.tests.TestViews.is_home_html_rendered
         '''
         response = self.client.get('')
+        print("\nStatus code:", response.status_code)
+        self.assertEqual(response.status_code, 200)
+
+
+    def test_if_report_template_renders(self):
+        '''
+         python manage.py test checks.tests.TestViews.is_home_html_rendered
+        '''
+        response = self.client.get('report')
         print("\nStatus code:", response.status_code)
         self.assertEqual(response.status_code, 200)
