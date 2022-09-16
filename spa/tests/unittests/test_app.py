9cb537c2f1646e6911bca45a101b9356846b5710
aa-ag <aaron.aguerrevere@gmail.com>
2022-09-15 21:51:51 -0500

    Add test_that_debug_is_false

--- spa/tests/unittests/test_app.py
+++ spa/tests/unittests/test_app.py
@@ -7,3 +7,6 @@ import spa.settings as settings
 class TestViews(TestCase):
     def test_that_checks_is_in_app(self):
         self.assertTrue("checks" in settings.INSTALLED_APPS)
+
+    def test_that_debug_is_false(self):
+        self.assertTrue(settings.DEBUG==False)
