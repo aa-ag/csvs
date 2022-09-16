2da65a44990d27238f384f6d3a037a5196253b1b
aa-ag <aaron.aguerrevere@gmail.com>
2022-09-15 21:47:19 -0500

    Add test to make sure the checks app is installed

--- spa/tests/unittests/test_app.py
+++ spa/tests/unittests/test_app.py
@@ -3,3 +3,7 @@ from django.test import TestCase
 ### relative
 import spa.settings as settings
 
+### Unit tests
+class TestViews(TestCase):
+    def test_that_checks_is_in_app(self):
+        self.assertTrue("checks" in settings.INSTALLED_APPS)
